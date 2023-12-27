68909de113cbd58db710cc59a6ae4bfa77295847
curitis <curitis@mogul-tech.com>
2023-12-27 15:20:58 +0800

    根据游戏分类获取数据

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,24 @@
-import useGenres from "../hooks/useGenres";
-import { HStack, List, ListItem, Text, Image, Spinner } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
+import {
+  HStack,
+  List,
+  ListItem,
+  Image,
+  Spinner,
+  Button,
+} from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
+
+interface Props {
+  selectedGenre: Genre | null;
+  onSelectedGenre: (genre: Genre) => void;
+}
 
 /**
  * 游戏类型列表
  * @returns
  */
-function GenreList() {
+function GenreList({ selectedGenre, onSelectedGenre }: Props) {
   const { data, error, isLoading } = useGenres();
 
   if (error) return null;
@@ -21,7 +33,14 @@ function GenreList() {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Text fontSize="lg">{genre.name}</Text>
+            <Button
+              fontSize="lg"
+              variant="link"
+              onClick={() => onSelectedGenre(genre)}
+              isActive={selectedGenre && selectedGenre.id == genre.id}
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>
       ))}
